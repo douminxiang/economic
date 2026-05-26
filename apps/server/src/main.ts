8ba6556949dae80794efcc24a7174f789faee63e
dmz <2421872737@qq.com>
2026-05-26 19:03:08 +0800

    feat: add common guards, filters, interceptors, decorators

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors();
+
+  await app.listen(3000);
+  console.log('Server running on http://localhost:3000');
 }
 bootstrap();
